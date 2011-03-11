dcfe1122ab8b2aa5830a1a75b2ecb3b42f0d8cfd
Samvel Khalatyan <ksamdev@gmail.com>
2011-03-11 11:11:12 -0600

    Add performance measurement script

---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+files=1
+if [[ 0 -lt $# ]]
+then
+    files=$1
+fi
+
+events=10000
+if [[ 1 -lt $# ]]
+then
+    events=$2
+fi
+
+rm ./data*.pb
+rm ./data*.root
+
+echo Generate $files files with $events events in each
+echo
+
+echo WRITING
+echo =======
+echo
+
+time ./bin/write pb $files $events && echo ---===---  && time ./bin/write root $files $events
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+echo
+echo READING
+echo =======
+echo
+
+time ./bin/read data*.pb && echo ---===--- && time ./bin/read data*.root
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+exit 0
