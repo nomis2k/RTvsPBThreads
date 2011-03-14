4fd8c0286a09d2b0528c1711dca3159c5d9bbb76
Samvel Khalatyan <ksamdev@gmail.com>
2011-03-14 15:48:15 -0500

    Add ProtoBuf test script

---
+++ test_pb.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+files=1
+if [[ 0 -lt $# ]]
+then
+    files=$1
+fi
+
+events=10000
+if [[ 1 -lt $# ]]
+then
+    events=$2
+fi
+
+rm ./data*.pb &> /dev/null
+
+echo Generate $files files with $events events in each
+echo
+
+echo WRITING
+echo =======
+echo
+
+time ./bin/write pb $files $events
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+echo
+echo READING
+echo =======
+echo
+
+time ./bin/read data*.pb
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+echo
+echo "READING (MULTITHREADS)"
+echo =====================
+echo
+
+time ./bin/read --threads -1 data*.pb
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+exit 0
