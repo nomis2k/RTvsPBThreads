1716b8862c8f910b61a901ac30ce2d11ba3f024c
Samvel Khalatyan <ksamdev@gmail.com>
2011-03-14 16:05:10 -0500

    Add root test script

---
+++ test_rt.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+files=1
+if [[ 0 -lt $# ]]
+then
+    files=$1
+fi
+
+events=10000
+if [[ 1 -lt $# ]]
+then
+    events=$2
+fi
+
+rm ./data*.root &> /dev/null
+
+echo Generate $files files with $events events in each
+echo
+
+echo WRITING
+echo =======
+echo
+
+time ./bin/write root $files $events
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+echo
+echo READING
+echo =======
+echo
+
+time ./bin/read data*.root
+if [[ 0 -ne $? ]]
+then
+    exit 1
+fi
+
+exit 0
